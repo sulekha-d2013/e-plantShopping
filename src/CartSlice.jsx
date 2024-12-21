8360321673feb8fbd459b45676d9a28e5f23f9dd
sulekha-d2013 <sulekha.d2013@gmail.com>
2024-12-21 23:34:56 +0530

    Add files via upload

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const plant = state.items.find(
+                (item) => item.name === action.payload.name
+            );
 
-    
+            if (plant) {
+                plant.quantity++;
+            } else {
+                state.items.push({ ...action.payload, quantity: 1 });
+            }
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter(
+                (item) => item.name !== action.payload.name
+            );
+        },
+        updateQuantity: (state, action) => {
+            const plant = state.items.find(
+                (item) => item.name === action.payload.name
+            );
+
+            if (plant) {
+                plant.quantity = action.payload.quantity;
+            }
+        },
     },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
